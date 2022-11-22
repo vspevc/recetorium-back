6c5f4914315f2d13062bdb8ee11c1f1db901dd14
Enric Vazquez <vspevc@gmail.com>
2022-11-23 00:33:39 +0100

    Add cors whitelist policy to server

--- src/CustomError/serverErrorMessages.ts
+++ src/CustomError/serverErrorMessages.ts
@@ -2,6 +2,7 @@ import type CustomErrorStructure from "./types";
 
 interface ServerCustomErrorsStructure {
   notFoundErrorMessage: (path: string) => CustomErrorStructure;
+  unauthorizedCorsOrigin: (requestOrigin: string) => CustomErrorStructure;
   unknownServerErrorMessage: string;
 }
 
@@ -10,6 +11,11 @@ const serverCustomErrors: ServerCustomErrorsStructure = {
     message: `Endpoint not found: ${path}`,
     publicMessage: "Endpoint not found",
     statusCode: 404,
+  }),
+  unauthorizedCorsOrigin: (requestOrigin) => ({
+    message: `Unauthorized cors origin: ${requestOrigin}`,
+    publicMessage: "Cross-Origin Request Blocked",
+    statusCode: 400,
   }),
   unknownServerErrorMessage: "Internal server error",
 };
