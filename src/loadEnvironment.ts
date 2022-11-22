b3adf69ea62ec2bc29dc697d5edbf96ba72dc91f
Enric Vazquez <vspevc@gmail.com>
2022-11-22 09:25:40 +0100

    Add database connection

--- src/loadEnvironment.ts
+++ src/loadEnvironment.ts
@@ -1,14 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const { PORT: port } = process.env;
+const { PORT: port, MONGODB_URL: mongoDatabaseUrl } = process.env;
 
 interface EnvironmentStructure {
   port: number;
+  mongoDatabaseUrl: string;
 }
 
 const environment: EnvironmentStructure = {
   port: +port || 4000,
+  mongoDatabaseUrl,
 };
 
 export default environment;
