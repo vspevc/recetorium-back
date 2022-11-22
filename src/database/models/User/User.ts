7f652671f698bab5ca9b5984f75475efdfd9355f
Enric Vazquez <vspevc@gmail.com>
2022-11-22 10:51:57 +0100

    Add User database model

---
+++ src/database/models/User/User.ts
@@ -0,0 +1,49 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  myRecipes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Recipe",
+    },
+  ],
+  favoriteRecipes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Recipe",
+    },
+  ],
+  scheduledRecipes: [
+    {
+      recipeId: {
+        type: Schema.Types.ObjectId,
+        ref: "Recipe",
+      },
+      scheduledDate: Number,
+    },
+  ],
+  createAt: {
+    type: Number,
+    default: Date.now(),
+  },
+});
+
+const User = model("User", userSchema, "users");
+
+export default User;
