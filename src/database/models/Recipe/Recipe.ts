a03a6a85c9f904e7be9f5e3b12e9466b0a21330d
Enric Vazquez <vspevc@gmail.com>
2022-11-28 17:55:18 +0100

    Add Recipe model

---
+++ src/database/models/Recipe/Recipe.ts
@@ -0,0 +1,72 @@
+import { model, Schema } from "mongoose";
+
+const ingredientsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: String,
+    required: true,
+  },
+});
+const stepsSchema = new Schema({
+  step: {
+    type: String,
+    required: true,
+  },
+  order: {
+    type: Number,
+    required: true,
+  },
+});
+
+const recipeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  urlSlug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  type: {
+    type: String,
+    enum: ["desayuno", "almuerzo", "comida", "cena", "postre"],
+    required: true,
+  },
+  ingredients: {
+    type: [ingredientsSchema],
+    required: true,
+  },
+  steps: {
+    type: [stepsSchema],
+    required: true,
+  },
+  elaborationTime: {
+    type: String,
+    maxLenght: 8,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  backupImage: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Number,
+    default: Date.now(),
+  },
+});
+
+const Recipe = model("Recipe", recipeSchema, "recipes");
+
+export default Recipe;
