a6a0c571e0731679cfeeaff8834a80951ec78344
Enric Vazquez <vspevc@gmail.com>
2022-12-03 11:03:38 +0100

    Fix regex issues on slugify

--- src/utils/slug/slugify.ts
+++ src/utils/slug/slugify.ts
@@ -1,7 +1,7 @@
 const slugify = (toSlug: string): string =>
   toSlug
     .toLowerCase()
-    .replace(/^\s+|\s+$/g, "")
+    .trim()
     .replace(/[áäâàãåÆæ]/g, "a")
     .replace(/[þÞß]/g, "b")
     .replace(/[čçć]/g, "c")
@@ -11,11 +11,11 @@ const slugify = (toSlug: string): string =>
     .replace(/[ňñ]/g, "n")
     .replace(/[óöòôõøð]/g, "o")
     .replace(/[řŕ]/g, "r")
-    .replace(/[š]/g, "s")
-    .replace(/[ť]/g, "t")
+    .replace(/š/g, "s")
+    .replace(/ť/g, "t")
     .replace(/[úůüùû]/g, "u")
     .replace(/[ýÿ]/g, "y")
-    .replace(/[ž]/g, "z")
+    .replace(/ž/g, "z")
     .replace(/[·/_,:;]/g, "-")
     .replace(/[^a-z0-9 -]/g, "")
     .replace(/\s+/g, "-")
