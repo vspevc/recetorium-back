dd8088a3ab6abd468e203e3d4152db432d6b2f1c
Enric Vazquez <vspevc@gmail.com>
2022-12-05 21:33:03 +0100

    Fix Joi validation allow axios form data

--- src/server/middleware/validation/createRecipesValidation.ts
+++ src/server/middleware/validation/createRecipesValidation.ts
@@ -4,38 +4,32 @@ const createRecipesValidation = {
   body: Joi.object({
     name: Joi.string().required(),
     author: Joi.string().required(),
-    types: Joi.array()
-      .items(
-        Joi.object({
-          name: Joi.string().valid(
-            "desayuno",
-            "almuerzo",
-            "comida",
-            "cena",
-            "postre"
-          ),
-        })
-      )
-      .required(),
-    ingredients: Joi.array()
-      .items(
-        Joi.object({
-          name: Joi.string(),
-          quantity: Joi.string(),
-        })
-      )
-      .required(),
-    steps: Joi.array()
-      .items(
-        Joi.object({
-          step: Joi.string(),
-          order: Joi.number(),
-        })
-      )
-      .required(),
+    types: Joi.array().items(
+      Joi.object({
+        name: Joi.string().valid(
+          "desayuno",
+          "almuerzo",
+          "comida",
+          "cena",
+          "postre"
+        ),
+      })
+    ),
+    ingredients: Joi.array().items(
+      Joi.object({
+        name: Joi.string(),
+        quantity: Joi.string(),
+      })
+    ),
+    steps: Joi.array().items(
+      Joi.object({
+        step: Joi.string(),
+        order: Joi.number(),
+      })
+    ),
     elaborationTime: Joi.string().max(13).required(),
     image: Joi.boolean().valid(true),
-  }),
+  }).unknown(true),
 };
 
 export default createRecipesValidation;
